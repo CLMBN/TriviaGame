c88955c6f45a95eb036ef98b2abd30f3dd762f59
CLMBN <juanmiguel.ossa+git@gmail.com>
2017-02-17 10:48:06 -0500

    initial checkin base html layout and js for start screen

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+// Create start button and home screen
+
+function mainScreen() {
+		startGame = "<p class='text-center main-button-container'><a class='btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+		$(".mainGame").html(startGame);
+	}
+
+mainScreen();
+
+})
